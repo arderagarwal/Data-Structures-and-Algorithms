82b2177e1371dacb03ed780a33cddf67fd99fb53
Adarsh Agarwal <adarshagarwal@MacBook-Pro.local>
2020-10-14 19:08:51 +0530

    Added the Cpp program of Basic Commands

---
+++ Basics/BasicCommands.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	// to print we use cout
+	cout<<"Hello World";
+	// rules for declaring a variable:
+	// 	All variable names must begin with a letter of the alphabet or an underscore
+	// 	Variable names are case sensitive
+	// declaring a variable
+	int var1;
+	// Taking input of a variable
+	cin>>var1;
+	//Displaying the variable 
+	cout<<var1;
+	//on sucessful execution of the code the main function would return 0
+	return 0;
+}
