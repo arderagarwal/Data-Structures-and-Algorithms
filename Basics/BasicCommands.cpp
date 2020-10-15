63531e40ecfd21c41b2c36540cf49335d98338eb
Adarsh Agarwal <adarshagarwal@MacBook-Pro.local>
2020-10-15 21:14:42 +0530

    Added command for arithmetic operations

--- Basics/BasicCommands.cpp
+++ Basics/BasicCommands.cpp
@@ -11,7 +11,18 @@ int main(){
 	// Taking input of a variable
 	cin>>var1;
 	//Displaying the variable 
-	cout<<var1;
+	cout<<var1<<endl;
+    //Arithmetic Operators:
+    // + Addition
+    cout<<var1+var1<<endl;
+    // - Subtraction
+    cout<<var1-5<<endl;
+    // * Multiplication
+    cout<<var1*5<<endl;
+    // * Division
+    cout<<var1/5<<endl;
+    // % modulus to find remainder
+    cout<<var1%3;
 	//on sucessful execution of the code the main function would return 0
 	return 0;
 }
