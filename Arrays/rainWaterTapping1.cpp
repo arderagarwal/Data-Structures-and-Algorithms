594b63bf23c12db665798968224d2a53b64175e7
Adarsh Agarwal <adarshagarwal8@gmail.com>
2020-12-20 21:57:55 +0530

    Added interview bit solution for the problem

---
+++ Arrays/rainWaterTapping1.cpp
@@ -0,0 +1,20 @@
+int Solution::trap(const vector<int> &A) {
+    vector<int> B(A.size(),0);
+    vector<int> C(A.size(),0);
+    int maxl=INT_MIN;
+    int maxr=INT_MIN;
+    for(int i=0; i<A.size();i++){
+        B[i]=max(maxl,A[i]);
+        maxl=B[i];
+        C[A.size()-i-1]=max(maxr,A[A.size()-i-1]);
+        maxr=C[A.size()-i-1];
+    }
+    int ans=0;
+    for(int i=0;i<A.size();i++){
+        int a= (min(B[i],C[i])-A[i]);
+       if(a>0){
+           ans=ans+a;
+       }
+    }
+    return ans;
+}
