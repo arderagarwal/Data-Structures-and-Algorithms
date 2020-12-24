52d83356a83127329b8b4d11e3687793be243405
Adarsh Agarwal <adarshagarwal8@gmail.com>
2020-12-24 13:54:46 +0530

    Added O(N^2) approach for Max Subarray Sum Problem

--- Arrays/maxSubarraySum.cpp
+++ Arrays/maxSubarraySum.cpp
@@ -4,10 +4,20 @@ int main(){
     int n;
     cin>>n;
     int * arr = new int [n];
+    int * pre = new int [n+1];
+    pre[0]=0;
     for(int i=0; i<n;i++){
         cin>>arr[i];
+        pre[i+1]=pre[i]+arr[i];
     }
     int ans=INT_MIN;
+    for(int i=0 ; i<n+1;i++){
+        for(int j=i+1; j<n+1;j++){
+            ans=max(ans,pre[j]-pre[i]);
+        }
+    }
+    cout<<ans<<endl;
+    ans=INT_MIN;
     for(int start=0; start<n ; start++){
         for(int end=start; end<n; end++){
             int sum=0;
