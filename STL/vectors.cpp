0e010657be2adca594a83636f1b448bd4a5567fc
Adarsh Agarwal <adarshagarwal8@gmail.com>
2020-12-29 21:13:34 +0530

    Added comments to the code

--- STL/vectors.cpp
+++ STL/vectors.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main(){
     vector<int> vec;
     for(int i=0; i<10; i++){
-        vec.push_back(i);
+        vec.push_back(i);   //for inserting element at the back of vector
     }
-    vec.pop_back();
-    vec.front();
-    vec.back();
-    vec.begin();
-    vec.end();
+    vec.pop_back();   //for removing element from the back
+    vec.front();       // for getting element at front of vector
+    vec.back();         // for getting element at back of vector
+    vec.begin();        //for getting address of begining of the vector
+    vec.end();          // for getting address at the end of vector
 }
