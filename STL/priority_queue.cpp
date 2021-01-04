0ad5c9559a2fd0ae60ab3271049a5bf86b13baec
Adarsh Agarwal <adarshagarwal8@gmail.com>
2021-01-04 22:17:48 +0530

    Added comments to the  code

--- STL/priority_queue.cpp
+++ STL/priority_queue.cpp
@@ -1,10 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    priority_queue<int> pq;
-    pq.push(1);
+    priority_queue<int> pq; //initialsing a max priority queue
+    pq.push(1);             //inserting elements in priority_queue
     pq.push(2);
     pq.push(3);
     pq.push(4);
-    cout<<pq.top();
+    cout<<pq.top();         //finding element with highest priority
 }
